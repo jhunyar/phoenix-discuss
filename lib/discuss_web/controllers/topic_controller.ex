29d097a707d4fa8a9767be2333e0e8b6142b0f37
James Hunyar <james@homegauge.com>
2019-07-24 00:05:51 -0400

    Add create edit update functions

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,6 +2,13 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   
   alias Discuss.Topic
+  alias Discuss.Repo
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render(conn, "index.html", topics: topics)
+  end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -10,6 +17,36 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-     
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, _topic} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} -> 
+        render(conn, "new.html", changeset: changeset)
+     end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    changeset = Repo.get(Topic, topic_id)
+    |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset
+    end
   end
 end
