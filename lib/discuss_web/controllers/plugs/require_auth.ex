af12cd2b3affc57885bba7c6d2469fc52a835e18
James Hunyar <james@homegauge.com>
2019-07-30 23:56:07 -0400

    Add sockets and comments

--- lib/discuss_web/controllers/plugs/require_auth.ex
+++ lib/discuss_web/controllers/plugs/require_auth.ex
@@ -1,11 +1,7 @@
 defmodule Discuss.Plugs.RequireAuth do
   import Plug.Conn
   import Phoenix.Controller
-<<<<<<< HEAD
-
-=======
   
->>>>>>> 871c75462ae086e7ea74933b2dee59f4d490f185
   alias DiscussWeb.Router.Helpers
 
   def init(_params) do
--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -12,6 +12,11 @@ defmodule DiscussWeb.TopicController do
     render(conn, "index.html", topics: topics)
   end
 
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    render(conn, "show.html", topic: topic)
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -73,18 +78,5 @@ defmodule DiscussWeb.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: Routes.topic_path(conn, :index))
-  end
-
-  def check_topic_owner(conn, _params) do
-    %{params: %{"id" => topic_id}} = conn
-
-    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You cannot edit that")
-      |> redirect(to: Routes.topic_path(conn, :index))
-      |> halt()
-    end
   end
 end
